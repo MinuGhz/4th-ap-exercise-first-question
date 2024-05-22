327486fd5e63cb7f291ceec4fa3b2e12003d8bc2
MinuGhz <minooghandriz@gmail.com>
2024-05-22 13:14:51 +0330

    operator <<, get_person,

--- person.cpp
+++ person.cpp
@@ -1,5 +1,34 @@
-//
-// Created by IT CITY on 5/20/2024.
-//
-
 #include "person.h"
+#include "address.h"
+
+
+person::person() {
+    name = '\0';
+    id = '\0';
+}
+
+person::person(string name, string ID, address &ADD) {
+    this->name = name;
+    id = ID;
+    Address.country = ADD.country;
+    Address.city = ADD.city;
+    Address.street = ADD.street;
+}
+
+person::person(const person& P) {
+    name = P.name;
+    id = P.id;
+    Address.country = P.Address.country;
+    Address.city = P.Address.city;
+    Address.street = P.Address.street;
+}
+
+void person::get_person() {
+    cout<<"name: "<<name<<", id: "<<id<<", "<<Address;
+}
+
+ostream& operator << (ostream& output ,const person& Person){
+output<<Person.get_person();
+
+return output;
+}
--- person.h
+++ person.h
@@ -1,16 +1,27 @@
-//
-// Created by IT CITY on 5/20/2024.
-//
+
 
 #ifndef SERI4_PERSON_H
 #define SERI4_PERSON_H
 
-
+#include "address.h"
+#include "iostream"
+using namespace std;
 
 class person {
+private:
+    string name , id;
+    address Address;
 
+public:
+    person(string, string , address&);
+    person();
+    person(const person&);
+
+    void get_person();
+
+    friend address;
+    friend ostream& operator << (ostream& , const person&);
 };
-
 
 
 #endif //SERI4_PERSON_H
